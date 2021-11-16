bc5040aa816205f528acbb7ed04e715120974e8b
uliana fomina <6758599@gmail.com>
2021-11-17 01:31:01 +0300

    Updates

---
+++ precache-manifest.ae00341393028b1b95b2ef0e93d9cfa2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49e6c2d8e88120a1e9c3d14ac76ecfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5aefa4b757c5238f87c",
+    "url": "/static/css/main.f8837ef4.chunk.css"
+  },
+  {
+    "revision": "70120c9335eb043ad80c",
+    "url": "/static/js/2.d0ed85c6.chunk.js"
+  },
+  {
+    "revision": "c5aefa4b757c5238f87c",
+    "url": "/static/js/main.cd851b6e.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
